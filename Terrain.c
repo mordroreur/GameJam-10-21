c6e6fac1ae12e331e5e88cf0d4797923d759a729
Mathieu Laurencot <mathieu.laurencot@etu.uca.fr>
2021-10-23 14:39:54 +0200

    Terrain

--- Terrain.c
+++ Terrain.c
@@ -49,7 +49,7 @@ salle getKnownSalle(int n){
   case 0 :
     for(int i = 0; i < TAILLE_X_SALLE; i++){
       for(int j = 0; j < TAILLE_Y_SALLE; j++){
-	if(j != 20){
+	if(j != 15){
 	  s.terrain[i][j] = 0;
 	}else {
 	  s.terrain[i][j] = 1;
